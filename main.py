90d7229409452f38fdd2311f9a2ee827ebbe7951
Jevon Thompson <104575457+JevonThompsonx@users.noreply.github.com>
2024-10-15 21:19:38 -0700

    Added error handling
    - Check if guess is higher or lower than guessmax or min
    - check if guess is a number

--- main.py
+++ main.py
@@ -22,31 +22,35 @@ while PLAYING is True:
     try:
         if int(guessMax) < int(guessMin):
             raise MaxTooSmallError("Max is too small")
-
+        guessMin = int(guessMin)
+        guessMax = int(guessMax)
         number_to_guess = random.randint(int(guessMin),int(guessMax))
         print(f"\nNumber to guess {number_to_guess}")
         GUESSING = True
         while GUESSING is True:
             guess = input("Take a guess: ")
             try:
+                guess = int(guess)
+                match guess:
+                    case guess if guess > guessMax:
+                        print("\nGuess is too high")
+                    case guess if guess < guessMin:
+                        print("\nGuess is too low")
+
                 if is_integer(guess) is False:
                     raise ValueError("Error! guess failed number check")
-
-                guess = int(guess)
-                print(guess)
-                if guess == number_to_guess:
-                    print('yay u win')
+                GUESS = int(guess)
+                if GUESS == number_to_guess:
+                    print('\nyay u win\n')
                     GUESSING = False
                 else:
-                    print('Wrong. Please try again')
+                    print('\nWrong. Please try again\n')
             except ValueError:
-                print('\nError with guess')
+                print('\nYour guess needs to be a number')
     except ValueError:
         if is_integer(guessMax) is False:
             print('\nmax not a num')
         elif is_integer(guessMin) is False:
             print('\nmin not a num')
     except MaxTooSmallError:
-        print(f"Can\'t work with a max({guessMax}) that is smaller than the min({guessMin})")
-    finally:
-        print("\nLet\'s try that again")
+        print(f"\nCan\'t work with a max({guessMax}) that is smaller than the min({guessMin})")
